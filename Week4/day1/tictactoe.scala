8844183c966c9641d935b669812dc4c9d11e68b1
brice <brice.favre@gmail.com>
2013-12-21 15:07:23 +0100

    Update tictactoe.scala
    Now we try another game, this way is more clever.

--- Week4/day1/tictactoe.scala
+++ Week4/day1/tictactoe.scala
@@ -1,14 +1,58 @@
-class TicTacToe(val board: List[String]) {
-    def print {
+class TicTacToe() {
+    /**
+     * Define TicTacToe board, current player and start the game
+     */
+    var board= Array(Array("","",""),Array("","",""),Array("","",""))
+    var currentPlayer = "X"
+    println("Start game for player " + currentPlayer)
+    print()
+    
+    
+    /**
+     * Print board
+     */
+    def print() {
         board.foreach { arg => 
-            println(arg)
+            //arg.mkString(" - ")
+            println(arg.mkString(" | "))
         }
     }
+    
+    /**
+     * Play function
+     * X : X value in the board
+     * Y : Y value in the board
+     */
+    def playAt(X: Int,Y: Int) {
+        board(X)(Y) = currentPlayer
+        println (currentPlayer + " played and now the boards look like this")
+        print()
+        switchPlayer()
+    }
+    
+    /**
+     * Switch current player
+     */
+    def switchPlayer() {
+        if (currentPlayer == "X") {
+            currentPlayer = "O"
+        } else {
+            currentPlayer = "X"
+        }
+    }
+    
+    /**
+     * Reset game 
+     */
+    def reset() {
+        var board= Array(Array("","",""),Array("","",""),Array("","",""))
+        var currentPlayer = "X"
+        println("New game ")
+        print()
+    }
+    
 }
 
-val board = new TicTacToe(List
-    (List("X","X","O"),
-    List("0","X","O"),
-    List("0","0","X"))
-);
-board.print
+val game = new TicTacToe()
+game.playAt(1,1)
+game.playAt(0,1)
